a76bfd953f6f5bc826cf9702a38a97e697faf444
Yakshita <101899906+Yakshita1802@users.noreply.github.com>
2022-10-04 10:37:25 -0400

    Add files via upload

---
+++ input_validation.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+
+	int input;
+	printf ("please enter a number greater than 5 and less than 10\n");
+	scanf("%d", &input);
+
+	//TODO: Read the comments and complete the "TODO" instruction in line 15 to 
+	//to produce the expected behavior. The expected behavior of the program is 
+	//to print out valid data!:) only when the user enters 
+	//a number greater than 5 and less than 10. Any other number will cause the system to
+	//be stuck in the loop and continue to ask for inputs.
+	//Do NOT modify any other code. Change ONLY line 15
+
+	while(input <= 5 || input >= 10) { //TODO: please replace ??? with a compound conditional expression
+		printf ("invalid number\n");
+		printf ("please enter a number greater than 5 and less than 10\n");
+		scanf("%d", &input);
+	}
+	printf("valid data!:)\n");
+	return 0;
+}
